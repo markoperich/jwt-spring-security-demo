8b93ab74e98752749e687baeb996724f4bc1aee4
Marko <marko.perich@gmail.com>
2018-07-10 12:40:01 -0400

    corrije issue de sonar

--- src/main/java/org/zerhusen/config/WebSecurityConfig.java
+++ src/main/java/org/zerhusen/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public JwtAuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
+    public JwtAuthenticationTokenFilter authenticationTokenFilterBean() {
         return new JwtAuthenticationTokenFilter();
     }
 
